075e5c0c7f355aaf0d332b3a169254838d1ceb4d
kirannarayan098 <138458431+kirannarayan098@users.noreply.github.com>
2023-08-08 12:18:06 +0530

    sample is commited

---
+++ src/test/java/gitpracticesample/sample.java
@@ -0,0 +1,14 @@
+package gitpracticesample;
+
+import org.testng.annotations.Test;
+
+public class sample {
+	@Test
+	public void sample1()
+	{
+		System.out.println("hello this is my second commit");
+		
+		System.out.println("hi");
+	}
+
+}
