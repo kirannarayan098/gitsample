e474b0faef4e4531ce44230037b32a1c15e0cd33
kirannarayan098 <138458431+kirannarayan098@users.noreply.github.com>
2023-08-08 11:39:47 +0530

    demo is commited

---
+++ src/test/java/gitpracticesample/demo.java
@@ -0,0 +1,12 @@
+package gitpracticesample;
+
+import org.testng.annotations.Test;
+
+public class demo {
+	@Test
+	public void sample()
+	{
+		System.out.println("this is my first commit");
+	}
+
+}
